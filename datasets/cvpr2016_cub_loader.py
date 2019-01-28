ca818f665849e808b976bf71ff6cb3135ca6260c
Boris Oreshkin <boris@elementai.com>
2019-01-28 10:46:56 -0500

    First version of the loader that can handle both text and image

--- datasets/cvpr2016_cub_loader.py
+++ datasets/cvpr2016_cub_loader.py
@@ -3,12 +3,39 @@ import os
 from PIL import Image
 from tqdm import tqdm
 import numpy as np
+from torch.utils.serialization import load_lua
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 
 class cvpr2016CubLoader():
     """
     Loads the CVPR2016-CUB dataset
     """
+
+    def __init__(self, data_dir=DEFAULT_DIR, cub_dir=DEFAULT_CUB_DIR, split="train",
+                 img_target_size=299, img_border_size=16, max_text_len=30, vocab_size=10000):
+        """
+
+        :param data_dir: data in which the main dataset is stored
+        :param cub_dir: the folder inside the main dataset where the original CUB-2011 data are stored
+        :param split: name of the split, admissible names: train, test, val, trainval, all
+        """
+
+        self.data_dir = data_dir
+        self.split = split
+        self.cub_dir = cub_dir
+        self.img_target_size = img_target_size
+        self.img_border_size = img_border_size
+        self.max_text_len = max_text_len
+        self.vocab_size = vocab_size
+
+    def load(self):
+        print()
+        print('Loading split', self.split)
+        self._load_image_meta()
+        self._load_tokenized_text()
+        self._load_raw_images()
 
     def _load_split_class_ids(self):
         with open(os.path.join(self.data_dir, self.split + 'classes.txt')) as f:
@@ -19,7 +46,7 @@ class cvpr2016CubLoader():
         # Load image names
         with open(os.path.join(self.data_dir, self.cub_dir, 'images.txt')) as f:
             image_lines = f.read().splitlines()
-        # LOad train/val/test split file
+        # Load train/val/test split file
         split_class_ids = self._load_split_class_ids()
 
         self.image_ids = []
@@ -44,7 +71,7 @@ class cvpr2016CubLoader():
         assert len(set(self.image_classes) - set(split_class_ids)) == 0
 
     def _load_raw_images(self):
-        print("Load raw image data for split:", self.split)
+        print("Load raw image data for split", self.split)
         self.raw_images = []
         for image_path in tqdm(self.image_paths):
             im = Image.open(image_path)
@@ -52,26 +79,50 @@ class cvpr2016CubLoader():
             im = im.resize((int(scale_factor*im.size[0]), int(scale_factor*im.size[1])), Image.ANTIALIAS)
             self.raw_images.append(im)
 
-    def __init__(self, data_dir=DEFAULT_DIR, cub_dir=DEFAULT_CUB_DIR, split="train",
-                 img_target_size=299, img_border_size=16):
-        """
+    def _load_tokenized_text(self):
+        # # Load vocabulary
+        # self.vocab = load_lua(os.path.join(self.data_dir, 'vocab_c10.t7'))
+        # # Load tokenized text from the original dataset
+        # self.bow = load_lua(os.path.join(self.data_dir, 'bow_c10', '072.Pomarine_Jaeger.t7'))
+        # self.word = load_lua(os.path.join(self.data_dir, 'word_c10', '072.Pomarine_Jaeger.t7')).numpy().astype(dtype=np.int32)
+        self._build_vocabulary(vocab_size=self.vocab_size)
+        print()
+        print("Loading and tokenizing texts in split", self.split)
+        self.raw_texts = []
+        self.tokenized_texts = []
+        self.tokenized_text_lengths = []
+        for image_file_name, image_class in tqdm(zip(self.image_file_names, self.image_classes_txt)):
+            raw_text_lines = self._load_texts_of_an_imange(image_class, image_file_name)
+            tokenized_text_lines = self.tokenizer.texts_to_sequences(raw_text_lines)
+            tokenized_text_lengths = [min(len(x), self.max_text_len) for x in tokenized_text_lines]
+            tokenized_text_lines = pad_sequences(tokenized_text_lines, maxlen=self.max_text_len, padding='post')
+            self.raw_texts.append(raw_text_lines)
+            self.tokenized_texts.append(tokenized_text_lines)
+            self.tokenized_text_lengths.append(tokenized_text_lengths)
 
-        :param data_dir: data in which the main dataset is stored
-        :param cub_dir: the folder inside the main dataset where the original CUB-2011 data are stored
-        :param split: name of the split, admissible names: train, test, val, trainval, all
-        """
+    def _build_vocabulary(self, vocab_size=10000):
+        print('Building vocabulary')
+        # create the loader object, to be able to go over the 'all' split and create the vocabulary
+        all_loader = cvpr2016CubLoader(data_dir=self.data_dir, cub_dir=self.cub_dir, split='all')
+        all_loader._load_image_meta()
+        print("Loading all raw texts")
+        raw_texts = []
+        for image_file_name, image_class in tqdm(zip(all_loader.image_file_names, all_loader.image_classes_txt)):
+            text_lines = self._load_texts_of_an_imange(image_class, image_file_name)
+            raw_texts.extend(text_lines)
+        all_loader = None  # this is just for safety to avoid test data leaks
 
-        self.data_dir = data_dir
-        self.split = split
-        self.cub_dir = cub_dir
-        self.img_target_size = img_target_size
-        self.img_border_size = img_border_size
+        tokenizer = Tokenizer(num_words=vocab_size, filters='\'!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n')
+        tokenizer.fit_on_texts(raw_texts)
+        self.tokenizer = tokenizer
 
-        self._load_image_meta()
-        self._load_raw_images()
-
-
-        a = 1
+    def _load_texts_of_an_imange(self, image_class:str=None, image_file_name:str=None):
+        with open(os.path.join(self.data_dir, 'text_c10', image_class, image_file_name.split('.')[0] + '.txt')) as f:
+            text_lines = f.read().splitlines()
+        assert len(text_lines) == 10
+        return text_lines
 
 
-cvpr2016CubLoader(data_dir='cvpr2016_cub', cub_dir=DEFAULT_CUB_DIR)
+
+# loader = cvpr2016CubLoader(data_dir='cvpr2016_cub', cub_dir=DEFAULT_CUB_DIR)
+# loader.load_data()
