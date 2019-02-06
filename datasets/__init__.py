735b7ca59d9c55d773e04d572c09398afb9d5733
Boris Oreshkin <boris@elementai.com>
2019-02-06 11:34:49 -0500

    Adding methods in the base dataset class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,5 +7,13 @@ class Dataset:
     def next_batch(self, batch_size: int = 64, num_images: int = 2, num_texts: int = 5):
         pass
 
+    def next_batch_features(self, batch_size: int = 64, num_images: int = 2, num_texts: int = 5):
+        pass
+
+    def sequential_evaluation_batches(self, batch_size: int = 64, num_images: int = 2, num_texts: int = 5):
+        pass
+
+    def sequential_evaluation_batches_features(self, batch_size: int = 64, num_images: int = 2, num_texts: int = 5):
+        pass
 
 
