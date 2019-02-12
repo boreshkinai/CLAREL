5fd111193833862eb920beae4e2a0f5d690022f2
Boris Oreshkin <boris@elementai.com>
2019-02-12 18:51:29 +0000

    Created the baseclass for inception_v2 and inception_v3 loaders

--- common/pretrained_models.py
+++ common/pretrained_models.py
@@ -7,6 +7,8 @@ import argparse
 import pathlib
 import urllib.request as request
 from keras.utils import get_file
+from tensorflow.contrib.layers.python.layers import layers as layers_lib
+from tensorflow.python.ops import variable_scope
 
 slim = tf.contrib.slim
 
@@ -15,61 +17,81 @@ INCEPTION_V3_URL = "http://download.tensorflow.org/models/inception_v3_2016_08_2
 INCEPTION_V2_PATH = os.path.join(os.sep, 'mnt', 'datasets', 'public', 'research', 'cvpr2016_cub', 'inception_v2.ckpt')
 INCEPTION_V2_URL = "http://download.tensorflow.org/models/inception_v2_2016_08_28.tar.gz"
 
+class InceptionLoader:
+    
+    def __init__(self):
+        pass
+    
+    def __del__(self):
+        tf.reset_default_graph()
+        self.sess.close()
 
-class InceptionV3Loader:
-
-    def __init__(self, model_path=INCEPTION_V3_PATH, image_size=299):
-        get_file(INCEPTION_V3_PATH, INCEPTION_V3_URL, untar=True)
-        self.sess = tf.Session()
-        self.checkpoint_file = model_path
-
-        self.input_tensor = tf.placeholder(tf.float32, shape=(None, image_size, image_size, 3), name='input_image')
-        input_tensor = tf.image.resize_bilinear(self.input_tensor, size=[299, 299], align_corners=False)
+    def predict(self, images):
+        predict_values, logit_values, embedding = self.sess.run(
+            [self.end_points['Predictions'], self.logits, self.end_points['PreLogits']],
+            feed_dict={self.input_tensor: images})
+        embedding = np.reshape(embedding, (embedding.shape[0], embedding.shape[-1]))
+        return predict_values, logit_values, embedding
+    
+    def _get_session(self):
+        config = tf.ConfigProto(allow_soft_placement=True)
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+        
+    def _restore(self):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, self.checkpoint_file)
+        
+    def _get_inception_preprocessing(self, inception_size):
+        self.input_tensor = tf.placeholder(tf.float32, shape=(None, self.image_size, self.image_size, 3), name='input_image')
+        input_tensor = tf.image.resize_bilinear(self.input_tensor, size=[inception_size, inception_size], align_corners=False)
         scaled_input_tensor = tf.scalar_mul((1.0 / 255), input_tensor)
         scaled_input_tensor = tf.subtract(scaled_input_tensor, 0.5)
         self.scaled_input_tensor = tf.multiply(scaled_input_tensor, 2.0)
+        
+
+class InceptionV3Loader(InceptionLoader):
+
+    def __init__(self, model_path=INCEPTION_V3_PATH, image_size=299):
+        cache_dir = os.sep.join(model_path.split(os.sep)[:-1])
+        get_file(fname=model_path.split(os.sep)[-1], cache_dir=cache_dir, cache_subdir='',
+                 origin=INCEPTION_V3_URL, untar=True)
+        
+        self._get_session()
+        self.checkpoint_file = model_path
+        self.image_size = image_size
+        self._get_inception_preprocessing(inception_size=299)
 
         self.arg_scope = inception.inception_v3_arg_scope()
         with slim.arg_scope(self.arg_scope):
             self.logits, self.end_points = inception.inception_v3(
-                scaled_input_tensor, is_training=False, num_classes=1001, reuse=False)
-        saver = tf.train.Saver()
-        saver.restore(self.sess, self.checkpoint_file)
+                self.scaled_input_tensor, is_training=False, num_classes=1001, reuse=False)
+        self._restore()
+        
 
-    def predict(self, images):
-        predict_values, logit_values, embedding = self.sess.run(
-            [self.end_points['Predictions'], self.logits, self.end_points['PreLogits']],
-            feed_dict={self.input_tensor: images})
-        embedding = np.reshape(embedding, (embedding.shape[0], embedding.shape[-1]))
-        return predict_values, logit_values, embedding
-
-
-class InceptionV2Loader:
+class InceptionV2Loader(InceptionLoader):
 
     def __init__(self, model_path=INCEPTION_V2_PATH, image_size=299):
-        get_file(INCEPTION_V2_PATH, INCEPTION_V2_URL, untar=True)
-        self.sess = tf.Session()
+        cache_dir = os.sep.join(model_path.split(os.sep)[:-1])
+        get_file(fname=model_path.split(os.sep)[-1], cache_dir=cache_dir, cache_subdir='',
+                 origin=INCEPTION_V2_URL, untar=True)
+        
+        self._get_session()
         self.checkpoint_file = model_path
-
-        self.input_tensor = tf.placeholder(tf.float32, shape=(None, image_size, image_size, 3), name='input_image')
-        input_tensor = tf.image.resize_bilinear(self.input_tensor, size=[224, 224], align_corners=False)
-        scaled_input_tensor = tf.scalar_mul((1.0 / 255), input_tensor)
-        scaled_input_tensor = tf.subtract(scaled_input_tensor, 0.5)
-        self.scaled_input_tensor = tf.multiply(scaled_input_tensor, 2.0)
+        self.image_size = image_size
+        self._get_inception_preprocessing(inception_size=224)
 
         self.arg_scope = inception.inception_v2_arg_scope()
         with slim.arg_scope(self.arg_scope):
             self.logits, self.end_points = inception.inception_v2(
-                scaled_input_tensor, is_training=False, num_classes=1001, reuse=False)
-        saver = tf.train.Saver()
-        saver.restore(self.sess, self.checkpoint_file)
-
-    def predict(self, images):
-        predict_values, logit_values, embedding = self.sess.run(
-            [self.end_points['Predictions'], self.logits, self.end_points['PreLogits']],
-            feed_dict={self.input_tensor: images})
-        embedding = np.reshape(embedding, (embedding.shape[0], embedding.shape[-1]))
-        return predict_values, logit_values, embedding
+                self.scaled_input_tensor, is_training=False, num_classes=1001, reuse=False)
+            end_point = list(self.end_points.values())[-3]
+            kernel_size = end_point.get_shape().as_list()[-2]
+            with variable_scope.variable_scope('InceptionV2/Logits', reuse=True):
+                embedding = layers_lib.avg_pool2d(end_point, kernel_size, padding='VALID',
+                                                  scope='AvgPool_1a_%dx%d'%(kernel_size, kernel_size))
+            self.end_points['PreLogits'] = embedding
+        self._restore()
 
 
 IMAGE_MODELS = {"inception_v3": InceptionV3Loader, "inception_v2": InceptionV2Loader}
