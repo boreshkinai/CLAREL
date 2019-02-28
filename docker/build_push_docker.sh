b9b16b2d13312ac2a45d2573596c068a50d640aa
Boris Oreshkin <boris@elementai.com>
2019-02-28 11:24:43 +0000

    Updated docekr to tensorflow 1.12.0

--- docker/build_push_docker.sh
+++ docker/build_push_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile -t zeroshot-pairwise-tensorflow-1.8.0 .
-docker tag zeroshot-pairwise-tensorflow-1.8.0 images.borgy.elementai.lan/tensorflow/zeroshot-pairwise-tensorflow-1.8.0
-docker push images.borgy.elementai.lan/tensorflow/zeroshot-pairwise-tensorflow-1.8.0
+docker build -f Dockerfile -t zeroshot-pairwise-tensorflow-1.12.0 .
+docker tag zeroshot-pairwise-tensorflow-1.12.0 images.borgy.elementai.lan/tensorflow/zeroshot-pairwise-tensorflow-1.12.0
+docker push images.borgy.elementai.lan/tensorflow/zeroshot-pairwise-tensorflow-1.12.0
 chmod +x ../train.py
