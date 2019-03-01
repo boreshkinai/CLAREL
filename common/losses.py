eb5f6965a002599d07251ca0b0641bccbdffb6ed
Boris Oreshkin <boris@elementai.com>
2019-03-01 13:43:23 +0000

    Added RMSE loss. I figured out that it is really important to stop gradients on one of the modalities to make it converge. Otherwise it diverges outrageously.

--- common/losses.py
+++ common/losses.py
@@ -27,5 +27,22 @@ def get_mi_loss(modality1_dist, modality2_dist, flags):
     hy = get_entropy_kde(y, flags.mi_kernel_width)
     hxy = get_entropy_kde(z, flags.mi_kernel_width)
     
-    mi_loss = -hxy + hx + hy
-    return -mi_loss
+    mi = -hxy + hx + hy
+    perplexity = tf.pow(2.0, hx*hy)
+    return -tf.div_no_nan(mi, perplexity)
+
+
+def get_rmse_loss(modality1_dist, modality2_dist, flags):
+    lower_tril_idxs = np.column_stack(np.tril_indices(n=flags.train_batch_size, k=-1))
+    x = tf.expand_dims(tf.gather_nd(modality1_dist, lower_tril_idxs), axis=-1) 
+    y = tf.expand_dims(tf.gather_nd(modality2_dist, lower_tril_idxs), axis=-1)
+#     x_max = tf.reduce_max(tf.stop_gradient(x))
+#     y_max = tf.reduce_max(tf.stop_gradient(y))
+#     loss = tf.norm(tf.exp(x-x_max)-tf.exp(y-y_max)) / len(lower_tril_idxs)
+#     x = tf.stop_gradient(x)
+    y_const = tf.stop_gradient(y)
+    x_const = tf.stop_gradient(x)
+    loss1 = tf.div_no_nan( tf.square(tf.norm(x - y_const)), tf.norm(x) * tf.norm(y_const) )
+    loss2 = tf.div_no_nan( tf.square(tf.norm(x_const - y)), tf.norm(x_const) * tf.norm(y) )
+    return 0.5*loss1 + 0.5*loss2
+
