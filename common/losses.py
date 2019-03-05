b7a5b13b07751c1a54254b3f0e8760068e101f4f
Boris Oreshkin <boris@elementai.com>
2019-03-05 21:01:54 +0000

    Added loss based on the cross-classification between modalities. One modality provides labels for the other modality

--- common/losses.py
+++ common/losses.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
 
 
 
@@ -39,6 +40,13 @@ def get_dist_mtx(x):
     return diff
 
 
+def get_dist_mtx_xy(x, y):
+    diff = (tf.expand_dims(x, -1)-y)
+    diff = -tf.square(diff)
+    diff = tf.reduce_mean(diff, axis=len(x.shape)-1)
+    return diff
+
+
 def get_rmse_loss(modality1_dist, modality2_dist, flags):
     lower_tril_idxs = np.column_stack(np.tril_indices(n=flags.train_batch_size, k=-1))
     x = tf.expand_dims(tf.gather_nd(modality1_dist, lower_tril_idxs), axis=-1) 
@@ -48,4 +56,68 @@ def get_rmse_loss(modality1_dist, modality2_dist, flags):
     loss1 = tf.div_no_nan( tf.square(tf.norm(x - y_const)), tf.norm(x) * tf.norm(y_const) )
     loss2 = tf.div_no_nan( tf.square(tf.norm(x_const - y)), tf.norm(x_const) * tf.norm(y) )
     return 0.5*loss1 + 0.5*loss2
+
+
+@tf.custom_gradient
+def grad_invert(x):
+    def grad(dy):
+        return dy
+    return x, grad
+
+
+def get_cross_classifier_loss(features_modality1, features_modality2, flags, scope="cluster_loss"):
+    num_features = features_modality1.get_shape().as_list()[-1]
+    with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+        class_prototypes = tf.get_variable(name='class_prototypes', shape=(flags.cross_class_num_clusters, num_features), 
+                                           dtype=tf.float32, trainable=True, regularizer=None,
+                                           initializer=tf.initializers.random_uniform)
+        class_prototypes_norm = tf.norm(class_prototypes, axis=-1, keepdims=True)
+        class_prototypes = tf.div_no_nan(class_prototypes, class_prototypes_norm)
+        
+        class_prototypes_adversarial = class_prototypes
+        
+        tf.summary.scalar('class_prototypes/min_norm_prototypes', tf.reduce_min(class_prototypes_norm))
+        tf.summary.scalar('class_prototypes/max_norm_prototypes', tf.reduce_max(class_prototypes_norm))
+        
+        features_modality1_norm = tf.norm(features_modality1, axis=-1, keepdims=True)
+        features_modality2_norm = tf.norm(features_modality2, axis=-1, keepdims=True)
+        features_modality1 = tf.div_no_nan(features_modality1, features_modality1_norm)
+        features_modality2 = tf.div_no_nan(features_modality2, features_modality2_norm)
+        
+        tf.summary.scalar('class_prototypes/min_norm_mod1', tf.reduce_min(features_modality1_norm))
+        tf.summary.scalar('class_prototypes/max_norm_mod1', tf.reduce_max(features_modality1_norm))
+        tf.summary.scalar('class_prototypes/min_norm_mod2', tf.reduce_min(features_modality2_norm))
+        tf.summary.scalar('class_prototypes/max_norm_mod2', tf.reduce_max(features_modality2_norm))
+        
+        logits_modality1 = 100.0*get_dist_mtx_xy(features_modality1, tf.transpose(class_prototypes))
+        logits_modality2 = 100.0*get_dist_mtx_xy(features_modality2, tf.transpose(class_prototypes))
+        
+        logits_modality1_adversarial = 100.0*get_dist_mtx_xy(tf.stop_gradient(features_modality1), 
+                                                             tf.transpose(class_prototypes_adversarial))
+        logits_modality2_adversarial = 100.0*get_dist_mtx_xy(tf.stop_gradient(features_modality2), 
+                                                             tf.transpose(class_prototypes_adversarial))
+        
+        tf.summary.scalar('class_prototypes/min_logits_adversarial_mod1', tf.reduce_min(logits_modality1_adversarial))
+        tf.summary.scalar('class_prototypes/max_logits_adversarial_mod1', tf.reduce_max(logits_modality1_adversarial))
+        
+        classes_modality1 = tf.argmax(tf.stop_gradient(logits_modality1_adversarial), axis=-1)
+        classes_modality2 = tf.argmax(tf.stop_gradient(logits_modality2_adversarial), axis=-1)
+        
+        loss1 = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(labels=classes_modality1, logits=logits_modality2))
+        loss2 = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(labels=classes_modality2, logits=logits_modality1))
+        
+        accuracy_mod1_ref = slim.metrics.accuracy(tf.argmax(logits_modality2, -1), classes_modality1)
+        accuracy_mod2_ref = slim.metrics.accuracy(tf.argmax(logits_modality1, -1), classes_modality2)
+
+        tf.summary.scalar('class_prototypes/accuracy_mod1_ref', accuracy_mod1_ref)
+        tf.summary.scalar('class_prototypes/accuracy_mod2_ref', accuracy_mod2_ref)
+        
+    return 0.5*loss1 + 0.5*loss2
+    
+    
+
+
+
 
