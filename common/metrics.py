9fc079ef5b8b45b8bfe4134512dfb881dc97e11a
Boris Oreshkin <boris@elementai.com>
2019-03-20 19:02:02 +0000

    Implemented the Top-1 accuracy calculation according to the good, the bad, the agly

--- common/metrics.py
+++ common/metrics.py
@@ -37,7 +37,8 @@ def get_prototypes(embeddings, labels, vectors_per_protype):
     prototypes = {}
     for cl in classes:
         class_idxs = idxs[labels == cl]
-        class_idxs_protype = np.random.choice(class_idxs, size=vectors_per_protype, replace=False)
+        class_idxs_protype = np.random.choice(class_idxs, replace=False,
+                                              size=min(vectors_per_protype, len(class_idxs)))
         prototype_embeddings = embeddings[class_idxs_protype]
         prototypes[cl] = prototype_embeddings.mean(axis=0)
     return prototypes
@@ -50,7 +51,7 @@ def ap_at_k_prototypes(support_embeddings, query_embeddings, class_ids, k=50, nu
     metrics = {}
     for current_num_texts in num_texts:
         prototypes = get_prototypes(embeddings=support_embeddings, labels=class_ids, 
-                                  vectors_per_protype=current_num_texts)
+                                    vectors_per_protype=current_num_texts)
         kdtree_support = cKDTree(np.array(list(prototypes.values())))
         class_ids_support = np.array(list(prototypes.keys()))
     
@@ -81,3 +82,37 @@ def ap_at_k_prototypes(support_embeddings, query_embeddings, class_ids, k=50, nu
         metrics['Top-1 Acc,#sentences%d'%(10*current_num_texts)] = top1_acc
     
     return metrics
+
+
+def top1_gzsl(support_embeddings, query_embeddings, class_ids_support, class_ids_query, num_texts=[1, 2, 3, 5, 10, 20, 50]):
+    class_ids_query = np.array(class_ids_query)
+    class_ids_support = np.array(class_ids_support)
+    metrics = {}
+    for current_num_texts in num_texts:
+        prototypes = get_prototypes(embeddings=support_embeddings, labels=class_ids_support, 
+                                    vectors_per_protype=current_num_texts)
+        kdtree_support = cKDTree(np.array(list(prototypes.values())))
+        class_ids_prototypes = np.array(list(prototypes.keys()))
+    
+        # Measure generalized zero-shot classification accuracy
+        # for each sample from query set, identify the closest member of support set
+        # count the number of times the class of the member identified matches the class of the query
+        top1_acc_matches = {}
+        top1_acc_counts = {}
+        top1_acc = {}
+        for query_id, query_embedding in enumerate(tqdm(query_embeddings)):
+            actual_class = class_ids_query[query_id]
+            nns, nn_idxs = kdtree_support.query(query_embedding, k=1)
+            nearest_class = class_ids_prototypes[nn_idxs]
+            
+            top1_acc_matches[actual_class] = top1_acc_matches.get(actual_class, 0.0) + np.float(nearest_class == actual_class)
+            top1_acc_counts[actual_class] = top1_acc_counts.get(actual_class, 0.0) + 1
+            
+        for key in top1_acc_matches.keys():
+            top1_acc[key] = top1_acc_matches[key] / top1_acc_counts[key]
+            
+        top1_acc /= len(query_embeddings)
+        metrics['Top-1 Acc GZSL,#sentences%d'%(10*current_num_texts)] = np.array(list(top1_acc.values())).mean()
+    
+    return metrics
+
